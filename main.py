d0b71bb8daf306f00f2a0a0db4ceecb9e3837b97
Gabriel Caetano <gabrielvscaetano@gmail.com>
2025-06-28 21:35:42 -0300

    refact: reorganize main function and add semantic analysis implementation

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 from lexical_analyzer import LexicalAnalyzer
 from syntax_analyzer import SyntaxAnalyzer
+from semantic_analyzer import SemanticAnalyzer
 
 def main():
     if len(sys.argv) < 2:
         print("Uso: python main.py <arquivo>")
         sys.exit(1)
     filename = sys.argv[1]
-    la = LexicalAnalyzer()
-    tokens = la.tokenize_file(filename)
+    lexic = LexicalAnalyzer()
+    tokens = lexic.tokenize_file(filename)
     # print(tokens)
-    sa = SyntaxAnalyzer()
-    ast = sa.parse(tokens)
+    syntax = SyntaxAnalyzer()
+    ast = syntax.parse(tokens)
+    semantic = SemanticAnalyzer()
+    execTree = semantic.analyze(ast)
+    # print(execTree)
     
 
 if __name__ == "__main__":
